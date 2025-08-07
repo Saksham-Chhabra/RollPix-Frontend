1ccf17b7b86d5584c8ba32335e55e323d90e5820
Saksham <s.chhabra2005@gmail.com>
2025-08-08 01:42:15 +0530

    well

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].js`,
-        chunkFileNames: `[name].js`,
-        assetFileNames: `[name].[ext]`,
-      },
-    },
-  },
+  plugins: [react()],
 });
